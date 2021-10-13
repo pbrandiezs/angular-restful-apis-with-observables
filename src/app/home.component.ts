5569e8d30eb5ff5085b507950d4c32a21fd75d26
Perry Brandiezs <pbrandiezs@gmail.com>
2021-10-13 14:43:43 -0700

    feat: Implemented imperitive routing example

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,7 +1,18 @@
 import { Component } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
-  template:'<h1>Home</h1>'
+  template:`
+  <h1>Home</h1>
+  <button (click)="onClick()">Go to GitHub Users</button>
+  `
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private _router: Router) {}
+
+  onClick(){
+    this._router.navigate(['GitHub']);
+  }
+}
